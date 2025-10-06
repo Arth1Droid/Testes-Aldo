5d11cb3eea2a25984b387a6ad68a3682001f3979
davi07 <davibento010@gmail.com>
2025-10-06 18:15:14 -0300

    classe Exemplar

--- Exemplar.py
+++ Exemplar.py
@@ -14,7 +14,7 @@ class Exemplar:
 
         if self.__status in ['1', '2']:
             self.__status = '3'
-            self.__qtdeEmprestimos += 1
+            self.__qtdeEmprestimo += 1
             return True
         
         return False
@@ -31,3 +31,44 @@ class Exemplar:
                 self.__status = '1'
             return True
         return False
+    
+    def alterarStatus(self, novo_status: str) -> str:
+        if self.__status in ['4', '9']:
+            raise UnsupportedOperationException("Exemplar em status final não permite nenhuma operação")
+
+        status_atual = self.__status
+
+        if status_atual in ['0', '2']:
+            self.__status = '1'
+
+        elif status_atual == '1':
+            if novo_status in ['2', '5', '9']:
+                self.__status = novo_status
+            else:
+                raise ValueError("Novo status inválido para o status atual")
+
+        elif status_atual == '3':
+            if novo_status == '4':
+                self.__status = '4'
+            else:
+                raise ValueError("Novo status inválido para o status atual")
+
+        elif status_atual == '5':
+            if novo_status in ['1', '2']:
+                self.__status = novo_status
+            else:
+                raise ValueError("Novo status inválido para o status atual")
+
+        else:
+            return '0'
+
+        return self.__status
+    
+    def getIdExemplar(self) -> int:
+        return self.__idExemplar
+    
+    def getStatus(self) -> str:
+        return self.__status
+    
+    def getQtdeEmpretimo(self) -> int:
+        return self.__qtdeEmprestimo
